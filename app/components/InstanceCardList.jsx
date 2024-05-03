3969b436154da6bb2a6f7b55ee2ec7f26828ac0a
Chris Kazas <chriskazas.ck1@gmail.com>
2024-05-03 10:28:14 -0700

    paginate

--- app/components/InstanceCardList.jsx
+++ app/components/InstanceCardList.jsx
@@ -1,44 +1,24 @@
-
-import { useEffect, useState } from 'react'
 import InstanceCard from './InstanceCard';
+import { v4 as uuidv4 } from 'uuid';
 
-
-export default function InstanceCardList() {
-
-    const [instanceData, setInstanceData] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/instances/types');
-                if (!response.ok) {
-                    throw new Error(`HTTP error ${response.status}`);
-                }
-                const data = await response.json();
-
-                setInstanceData(data.instance_types);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
+export default function InstanceCardList({ instanceData }) {
 
     return (
         <main>
             {instanceData && instanceData.length > 0 ? (
-                instanceData.map((instance) => (
-                    <InstanceCard
-                        key={instance.cloud}
-                        instanceData={instance}
-                    />
-                ))
+                <>
+                    {instanceData.map((instance) => (
+                        <InstanceCard
+                            key={instance.cloud}
+                            instanceData={instance}
+                        />
+                    ))}
+
+                </>
             ) : (
                 <p>No instance data available.</p>
-            )}
-
-        </main>
-    )
+            )
+            }
+        </main >
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,51 +1,55 @@
 "use client"
 import Link from 'next/link'
 import {useEffect, useState} from 'react'
-import InstanceCard from './components/InstanceCard';
+import InstanceCardList from './components/InstanceCardList';
 
 
 export default function Home() {
 
   const [instanceData, setInstanceData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [instanceCount, setInstanceCount] = useState(0)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/instances/types');
-        if (!response.ok) {
-          throw new Error(`HTTP error ${response.status}`);
-        }
-        const data = await response.json();
+      const fetchData = async () => {
+          try {
+              const response = await fetch(`http://localhost:8080/instances/types`);
+              if (!response.ok) {
+                  throw new Error(`HTTP error ${response.status}`);
+              }
+              const responseData = await response.json();
 
-        setInstanceData(data.instance_types);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+              setInstanceData(responseData.instance_types.slice(instanceCount,instanceCount+10));
+              setTotalPages(Math.ceil(responseData.instance_types.length / 10));
+          } catch (error) {
+              console.error('Error fetching data:', error);
+          }
+      };
 
-    fetchData();
-  }, []);
+      fetchData();
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+      setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
+      setInstanceCount(instanceCount+10)
+  };
+
+  const handlePrevPage = () => {
+      setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
+      
+  };
 
 
   return (
     <main>
       <h2>Instance Types</h2>
-
-      {instanceData ? (
-  instanceData.map((instance) => (
-    <InstanceCard key={instance.cloud} instanceData={instance} />
-        ))
-      ) : (
-        <p>No instance data available.</p>
-      )}
-
-      <div className="flex justify-center my-8">
-        <Link href="/create">
-          <button className="btn-primary">Create Instance</button>
-        </Link>
+      <div className="grid grid-rows-4 grid-flow-col">
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>Previous</button>
+          <span>{currentPage} of {totalPages}</span>
+          <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
       </div>
-
-      
+      <InstanceCardList instanceData={instanceData}/>
     </main>
   )
 }
