75b8277284547c76818b1ea01a2f77ddd0ecb611
Chris Kazas <chriskazas.ck1@gmail.com>
2024-05-04 20:20:42 -0700

    fix header

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <nav>
             <img src="https://mintlify.s3-us-west-1.amazonaws.com/shadeform-67/logo/light-shadeform.svg" width="150" alt="light logo" />
-            <Link href='/'>Instance List</Link>
+            <Link href='/'>Instance Types</Link>
             <Link href='/instances'>Instances</Link>
         </nav>
     )
