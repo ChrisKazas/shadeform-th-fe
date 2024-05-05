13a5931be6a38d885f1d4de8ff72bacd4e222474
Chris Kazas <chriskazas.ck1@gmail.com>
2024-05-04 18:23:36 -0700

    fix lower boundary

--- app/components/InstanceCardList.jsx
+++ app/components/InstanceCardList.jsx
@@ -52,7 +52,7 @@ const InstanceCardList = () => {
     }
     const previous = (pageNumbers) => {
         if (currentPage != 1) {
-            setCurrentPage(currentPage + 1)
+            setCurrentPage(currentPage - 1)
         }
     }
 
